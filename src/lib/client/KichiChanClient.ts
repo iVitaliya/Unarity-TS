50324f62a802331a169517dabd30a370689606a0
Mochi <42318815+iAmMochi@users.noreply.github.com>
2020-10-23 13:31:38 +0200

    Beginning

---
+++ src/lib/client/KichiChanClient.ts
@@ -0,0 +1,9 @@
+// Packages...
+import {Client, Collection} from 'discord.js';
+
+// Files...
+import {} from '../index';
+
+class KichiChanClient extends Client {
+    
+}; 
---
+++ src/lib/utils/functions.ts
@@ -0,0 +1,1 @@
+export const searchQuery = (query: string, toSearch: string) => new RegExp(`.*${query.split(' ').join('.*')}.*`, 'gi').test(toSearch);
