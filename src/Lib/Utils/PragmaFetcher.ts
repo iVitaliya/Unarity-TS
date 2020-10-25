6b45d5e675e73440dd86daf1dc1908d86342d307
iAmMochi <42318815+iAmMochi@users.noreply.github.com>
2020-10-25 10:33:29 +0100

    Fetcher

--- src/Lib/Utils/PragmaFetcher.ts
+++ src/Lib/Utils/PragmaFetcher.ts
@@ -1,1 +1,20 @@
-// Finish this
+// Packages
+import {Message, User, Member, Role, TextChannel, Channel, Guild} from 'eris';
+
+// Files
+import {PragmaClient} from '../index';
+
+// Class
+class PragmaFetcher {
+    private client: PragmaClient;
+
+    constructor(client: PragmaClient) {
+        this.client = client;
+    }
+
+    // message(channel: TextChannel, value: any): Promise<Message | null> {
+    //     if (channel instanceof TextChannel) return value
+        
+    //     const messageProcessor = channel.
+    // }
+};
